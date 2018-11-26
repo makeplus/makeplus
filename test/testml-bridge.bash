38d5d43957d131852d324cff1d8a8f95515ae8c5
Ingy döt Net <ingy@ingy.net>
2018-11-26 14:17:54 -0800

    Add support for running MakePlus special commands

--- test/testml-bridge.bash
+++ test/testml-bridge.bash
@@ -1,21 +1,19 @@
 #! bash
 
 TestMLBridge.make-out() (
-  set -f
+  run-command "$@"
 
-  makefile=$(mktemp)
-  echo "$1" > "$makefile"
-
-  IFS=" " read -r -a args <<< "${2#make}"
-
-  stderr=$(mktemp)
-
-  $MAKE -f "$makefile" \
-    --no-print-directory \
-    "${args[@]}" \
-    2>"$stderr"
+  echo "$stdout"
 
   sed 's/^/# /' < "$stderr" >&2
+
+  rm "$makefile" "$stderr"
+)
+
+TestMLBridge.make-err() (
+  run-command "$@"
+
+  cat "$stderr"
 
   rm "$makefile" "$stderr"
 )
@@ -27,3 +25,21 @@ TestMLBridge.run() (
 
   "${cmd[@]}"
 )
+
+run-command() {
+  set -f
+
+  makefile=$(mktemp)
+  echo "$1" > "$makefile"
+
+  IFS=" " read -r -a args <<< "${2#make}"
+
+  stderr=$(mktemp)
+
+  stdout=$(
+    $MAKE -f "$makefile" \
+      --no-print-directory \
+      "${args[@]}" \
+      2>"$stderr" | grep -v 'Nothing to be done for'
+  )
+}
